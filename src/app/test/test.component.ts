253ff0e5086fe0cc4465ba4ae02b3f420b5c77a2
modkhalid <modkhalid5@gmail.com>
2018-10-31 16:24:57 +0530

    event in angular

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,43 +3,50 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template:`
-  		<p [class]="hh" [class.b]="some">hello world</p>
-  		<h1 [ngClass]="classObject" >this is test</h1>
-		<input bind-disabled="isDisabled" type="text" name="khslid" id={{myid}}>
-		<button (click)=fun()>clickme</button>
-
-
-		<hr>
-		<p [style.color]="colorName">modkhalid</p>
-		<p [style.color]="ifFlag?'green':'orange'">modkhalid</p>
-		<p [ngStyle]="obj">this is test heading</p>
-
+  		<button (mouseover)=onclickFun($event)>clickme</button>
+  		{{name}}
   `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
   constructor() { }
-  myid="khalid"
-  hh="a b c"
-  classObject={
-  	a:true,
-  	b:false,
-  	c:true,
-  	d:true
+  name:string
+  onclickFun(event){
+  	console.log(event)
+  	this.name=event.type;
   }
 
-  obj={
-  	color:"pink",
-  	textDecoration:"underline",
-  	fontSize:"100px"
-  }
-  colorName="red"
-  ifFlag=false
 
 
-  some=false
-  isDisabled:boolean=false
+
+
+
+
+  // binding in angular
+  // myid="khalid"
+  // hh="a b c"
+  // classObject={
+  // 	a:true,
+  // 	b:false,
+  // 	c:true,
+  // 	d:true
+  // }
+
+  // obj={
+  // 	color:"pink",
+  // 	textDecoration:"underline",
+  // 	fontSize:"100px"
+  // }
+  // colorName="red"
+  // ifFlag=false
+
+
+  // some=false
+  // isDisabled:boolean=false
+
+
+
   ngOnInit() {
   }
 
