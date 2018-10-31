fca9742020ec4ebc0bea9cea524de77a108ade71
modkhalid <modkhalid5@gmail.com>
2018-10-31 16:08:40 +0530

    binging in style [ngStyle]='objectName'

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,9 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template:`
-  		<p [class.hh]="hasError">hello world</p>
+  		<p [class]="hh" [class.b]="some">hello world</p>
+  		<h1 [ngClass]="classObject" >this is test</h1>
 		<input bind-disabled="isDisabled" type="text" name="khslid" id={{myid}}>
 		<button (click)=fun()>clickme</button>
+
+
+		<hr>
+		<p [style.color]="colorName">modkhalid</p>
+		<p [style.color]="ifFlag?'green':'orange'">modkhalid</p>
+		<p [ngStyle]="obj">this is test heading</p>
+
   `,
   styleUrls: ['./test.component.css']
 })
@@ -13,12 +21,24 @@ export class TestComponent implements OnInit {
 
   constructor() { }
   myid="khalid"
-  hh="hh"
-  hasError=true
-  fun(){
-  	// alert("modkhalid")
-  	this.hh="mm";
+  hh="a b c"
+  classObject={
+  	a:true,
+  	b:false,
+  	c:true,
+  	d:true
   }
+
+  obj={
+  	color:"pink",
+  	textDecoration:"underline",
+  	fontSize:"100px"
+  }
+  colorName="red"
+  ifFlag=false
+
+
+  some=false
   isDisabled:boolean=false
   ngOnInit() {
   }
